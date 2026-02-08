7571e7223d96858f3c549529bf6e6e8bfde4213d
Byte Chanokchon <94169899+bytechanokchon@users.noreply.github.com>
2026-02-08 16:07:09 +0700

    Create madlibs_game.py

---
+++ projects/004/madlibs_game.py
@@ -0,0 +1,14 @@
+# word game where you create a story by filling in black random words
+
+adjective1: str = input("Enter an adjective (description): ")
+adjective2: str = input("Enter an adjective (description): ")
+adjective3: str = input("Enter an adjective (description): ")
+
+noun1: str = input("Enter an noun (person, place, thing): ")
+
+verb1: str = input("Enter a verb ending with 'ing': ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
