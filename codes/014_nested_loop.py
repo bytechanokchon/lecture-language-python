50e6a982e64fe1d4999dfa560dea0f9229cf7634
Byte Chanokchon <94169899+bytechanokchon@users.noreply.github.com>
2026-02-23 22:12:04 +0700

    lec: nested loop

--- codes/014_nested_loop.py
+++ codes/014_nested_loop.py
@@ -1,1 +1,13 @@
 print("Teching content: Nested Loop")
+
+rows: int = int(input("Enter the # of rows: "))
+cols: int = int(input("Enter the # of cols: "))
+symbol: str = input("Enter the symbol to use: ")
+
+for count in range(rows):
+    for x in range(cols):
+        # การตั้งค่า end คือการระบุว่าตัวสุดท้ายของ string จะให้ลงท้ายด้วยอะไร
+        # โดยค่าเริ่มต้นคือ \n
+        print(symbol, end=" ")
+    print()
+
--- lectures/014_nested_loop.md
+++ lectures/014_nested_loop.md
@@ -1,1 +1,5 @@
 ### Nested Loop
+เป็นการที่มีลูปซ้อนลูปอีกทีหนึ่ง
+
+    outer loop:
+        inner loop:
